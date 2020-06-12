956f0eb2e723e9ac2b5008e13df4b309d9c24c05
Prashantsaini25 <64362251+Prashantsaini25@users.noreply.github.com>
2020-06-12 11:12:25 +0530

    cloudfront code with s3 bucket

---
+++ cf.tf
@@ -0,0 +1,101 @@
+resource "aws_s3_bucket" "pps-prince" {
+bucket = "pps-prince1"
+acl    = "private"
+tags = {
+Name        = "pps-prince"
+}
+}
+// Block Public Access
+resource "aws_s3_bucket_public_access_block" "s3Public" {
+bucket = "${aws_s3_bucket.pps-prince.id}"
+block_public_acls   = true
+block_public_policy = true
+restrict_public_buckets = true
+}
+
+locals {
+s3_origin_id = "princes3"
+}
+// Creating Origin Access Identity for CloudFront
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+comment = "pps-bucket"
+}
+resource "aws_cloudfront_distribution" "pps" {
+origin {
+domain_name = "${aws_s3_bucket.pps-prince.bucket_regional_domain_name}"
+origin_id   = "${local.s3_origin_id}"
+s3_origin_config {
+
+origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+}
+}
+enabled             = true
+is_ipv6_enabled     = true
+comment             = "pps-access"
+default_cache_behavior {
+allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+cached_methods   = ["GET", "HEAD"]
+target_origin_id = "${local.s3_origin_id}"
+forwarded_values {
+query_string = false
+cookies {
+forward = "none"
+}
+}
+viewer_protocol_policy = "allow-all"
+min_ttl                = 0
+default_ttl            = 3600
+max_ttl                = 86400
+}
+# Cache behavior with precedence 0
+ordered_cache_behavior {
+path_pattern     = "/content/immutable/*"
+allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+cached_methods   = ["GET", "HEAD", "OPTIONS"]
+target_origin_id = "${local.s3_origin_id}"
+forwarded_values {
+query_string = false
+headers      = ["Origin"]
+cookies {
+forward = "none"
+}
+}
+min_ttl                = 0
+default_ttl            = 86400
+max_ttl                = 31536000
+compress               = true
+viewer_protocol_policy = "redirect-to-https"
+}
+# Cache behavior with precedence 1
+ordered_cache_behavior {
+path_pattern     = "/content/*"
+allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+cached_methods   = ["GET", "HEAD"]
+target_origin_id = "${local.s3_origin_id}"
+forwarded_values {
+query_string = false
+cookies {
+forward = "none"
+}
+}
+min_ttl                = 0
+default_ttl            = 3600
+max_ttl                = 86400
+compress               = true
+viewer_protocol_policy = "redirect-to-https"
+}
+price_class = "PriceClass_200"
+restrictions {
+geo_restriction {
+restriction_type = "blacklist"
+locations        = ["CA"]
+}
+}
+tags = {
+Environment = "production"
+}
+viewer_certificate {
+cloudfront_default_certificate = true
+}
+retain_on_delete = true
+}
